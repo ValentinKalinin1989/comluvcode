058bc1a83d399bc64325550e116e5b9bc209dcac
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2020-01-25 17:42:01 +0300

    Получение все объектов из БД.

---
+++ src/main/java/com/luv2code/hibernate/demo/entity/ReadStudentDemo.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadStudentDemo {
+    public static void main(String[] args) {
+
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        //create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // use the session to save Java object
+            // create a  student object
+            System.out.println("Creating new student object...");
+            Student tempStudent = new Student("Petr", "Willer", "petr@luv2code.come");
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the student object
+            System.out.println("Saving the student...");
+            session.save(tempStudent);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            //find out the student's id primary key
+            System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
+            //now get new session
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            //retrieve student based on the id: primary key
+            System.out.println("\nGetting student with id: " + tempStudent.getId());
+            Student myStudent = session.get(Student.class, tempStudent.getId());
+
+            //commit the transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
+
